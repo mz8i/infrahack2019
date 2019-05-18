83090460147301c67f77409dbb2dc9bd6df7fed5
Alexis <alexislui.dev@gmail.com>
2019-05-18 06:43:46 +0100

    feed draft

--- unlit-app/src/Components/feed-view.js
+++ unlit-app/src/Components/feed-view.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
-export default class FeedView extends Component {
-    render() {
-        return <div>
-            Feed view
-        </div>
-    }
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className='feed-container'>
+        <div className='image1'></div>
+
+        <p style={{ textAlign: 'center' }}>I see this bright lights every night I walk home</p>
+      </div>
+    );
+  }
 }
