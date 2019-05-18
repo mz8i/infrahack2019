0a6136dedc0fd0ca4d70b7e72740fb25d1bbf957
Maciek <maciej.ziarkowski@gmail.com>
2019-05-18 11:53:52 +0900

    Improve navigation and photo submission

--- unlit-app/src/App.js
+++ unlit-app/src/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 import { Test, FeedView, PhotoView, MapView } from "./Components"
 
 class App extends Component {
   render() {
     return (
-      <div>
         <Router>
           <Route path="/" exact component={Test} />
           <Route path="/feed" exact component={FeedView} />
           <Route path="/photo" exact component={PhotoView} />
           <Route path="/map" exact component={MapView} />
+          <nav>
+            <div>
+              <NavLink to='/feed' activeClassName='active-nav'>Feed</NavLink>
+            </div>
+            <div>
+              <NavLink to='/photo' activeClassName='active-nav'>Photo</NavLink>
+            </div>
+            <div>
+              <NavLink to='/map' activeClassName='active-nav'>Map</NavLink>
+            </div>
+          </nav>
         </Router>
-        <a href="feed">Feed</a> <br />
-        <a href="photo">Photo</a> <br />
-        <a href="map">Map</a> <br />
-      </div>
     );
   }
 }
